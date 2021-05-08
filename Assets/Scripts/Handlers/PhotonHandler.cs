6dd619c4a1bd80b5c7dbc7ea1ba28d3833fe6f4a
DanielMarcucci <45695314+DanielMarcucci@users.noreply.github.com>
2021-05-08 04:12:40 -0600

    Multijugador montado

---
+++ Assets/Scripts/Handlers/PhotonHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+public class PhotonHandler : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Cuatro", new RoomOptions { MaxPlayers = 5 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Car", new Vector3(Random.Range(-2, 2), (float)1.84, (float)9.46), Quaternion.identity);
+        //PhotonNetwork.Instantiate("Car", new Vector3(Random.Range(-5, 5), (float)1.84, Random.Range(-5, 5)), Quaternion.identity);
+    }
+}
